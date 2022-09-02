51bc8507149f686f12463a669f038e6405aca1f4
feliperb12 <feliperibeiroaraujo3@hotmail.com>
2022-09-02 16:56:31 -0300

    Criando o package config e criando perfil de teste

---
+++ src/main/java/com/felipe/sts/os/config/TestConfig.java
@@ -0,0 +1,23 @@
+package com.felipe.sts.os.config;
+
+
+import com.felipe.sts.os.service.DBService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.Profile;
+
+import java.util.Arrays;
+
+@Configuration
+@Profile("test")
+public class TestConfig {
+
+    @Autowired
+    private DBService dbService;
+
+    @Bean
+    public void instanciaDB() {
+        this.dbService.instanciaDB();
+    }
+}
---
+++ src/main/java/com/felipe/sts/os/service/DBService.java
@@ -0,0 +1,43 @@
+package com.felipe.sts.os.service;
+
+import com.felipe.sts.os.Repository.ClienteRepository;
+import com.felipe.sts.os.Repository.OSRepository;
+import com.felipe.sts.os.Repository.TecnicoRepository;
+import com.felipe.sts.os.domain.Cliente;
+import com.felipe.sts.os.domain.OS;
+import com.felipe.sts.os.domain.Tecnico;
+import com.felipe.sts.os.domain.enuns.Prioridade;
+import com.felipe.sts.os.domain.enuns.Status;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import java.util.Arrays;
+
+@Service
+public class DBService {
+    @Autowired
+    private TecnicoRepository tecnicoRepository;
+    @Autowired
+    private ClienteRepository clienteRepository;
+    @Autowired
+    private OSRepository osRepository;
+
+
+    public void instanciaDB() {
+        Tecnico t1 = new Tecnico(null, "Felipe Ribeiro",
+                "709.074.931-22", "(62) 9944-7291");
+
+        Cliente c1 = new Cliente(null, "Betina Campos",
+                "598.508.200-80", "(62) 9944-7561");
+
+        OS os1 = new OS(null, Prioridade.ALTA,
+                "Teste create OS", Status.ANDAMENTO, t1, c1);
+
+        t1.getList().add(os1);
+        c1.getList().add(os1);
+
+        tecnicoRepository.saveAll(Arrays.asList(t1));
+        clienteRepository.saveAll(Arrays.asList(c1));
+        osRepository.saveAll(Arrays.asList(os1));
+    }
+}
