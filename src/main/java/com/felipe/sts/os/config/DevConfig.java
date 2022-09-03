512729239effea5d3db3d9024c6fa70627dfc7dd
feliperb12 <feliperibeiroaraujo3@hotmail.com>
2022-09-02 22:13:33 -0300

    reajustes

--- src/main/java/com/felipe/sts/os/config/DevConfig.java
+++ src/main/java/com/felipe/sts/os/config/DevConfig.java
@@ -9,7 +9,7 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Profile;
 
 @Configuration
-@Profile("dev")
+//@Profile("dev")
 public class DevConfig {
 
     @Autowired
